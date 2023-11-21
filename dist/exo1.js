df6c3392cf46ae76578a39f64707507bbfd64068
KarimSekri <karim.sekri@gmail.com>
2023-11-21 18:09:41 +0100

    Ajout du .dockerignore

--- dist/exo1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exo1 = void 0;
-const exo1 = () => {
-    console.log('toto');
-};
-exports.exo1 = exo1;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request } from "express";
+import express from "express";
 import dotenv from "dotenv/config";
 
 import cors from 'cors';
